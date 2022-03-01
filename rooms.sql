0baaf8c03ff7b2b900370882d07156cf7c1b0121
HaLiLiX <sahar.clili12@gmail.com>
2022-03-01 16:34:39 +0200

    commit sql

---
+++ rooms.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [room] NCHAR(10) NULL, 
+    [availability] NCHAR(10) NULL, 
+    [date] NCHAR(10) NULL, 
+    [owner] NCHAR(10) NULL 
+)
