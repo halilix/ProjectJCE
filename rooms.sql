020925941ed1a2d21c0540ee901f64494ffa82fe
barak94 <barakza94@gmail.com>
2022-05-29 20:02:03 +0300

    delete all

--- rooms.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table1]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [room] NCHAR(10) NULL, 
-    [availability] NCHAR(10) NULL, 
-    [date] NCHAR(10) NULL, 
-    [owner] NCHAR(10) NULL 
-)
--- users.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table2]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [username] NCHAR(10) NULL, 
-    [password] NCHAR(10) NULL, 
-    [name] NCHAR(10) NULL, 
-    [phone] NCHAR(10) NULL, 
-    [mail] NCHAR(10) NULL
-)
